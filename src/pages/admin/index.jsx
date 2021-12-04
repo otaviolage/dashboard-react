8950a7121ed214b6dd9a8d84ea2f3fac02738be3
otaviolage <otaviolagedossantos@gmail.com>
2021-12-04 12:17:28 -0300

    job done

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -9,7 +9,7 @@ function Admin() {
       <Header />
       <div className="Admin">
         <div className="container">
-          <iframe width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=ae1dc8d7-0a73-4291-9fa7-9cc6aa4fffca&autoAuth=true&ctid=59d4f249-0520-46cf-b6b2-873d5da543fe&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+          <iframe width="1280" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=ae1dc8d7-0a73-4291-9fa7-9cc6aa4fffca&autoAuth=true&ctid=59d4f249-0520-46cf-b6b2-873d5da543fe&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
           <div className="others"></div>
         </div>
       </div>
