c7566e971758de01a6461548b4d8d19c84cb1bf4
otaviolage <otaviolagedossantos@gmail.com>
2021-12-04 23:45:19 -0300

    home format fixes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,30 +1,26 @@
+import { useEffect } from "react";
+import Product from "../../components/Product";
+import { useCart } from "../../hooks/Cart";
+import Header from "../../components/Header";
+import data from "../../data/data.json";
+import "./style.css";
 
-import { useEffect } from 'react';
-import Product from '../../components/Product'; 
-import {useCart} from '../../hooks/Cart';
-import Header from '../../components/Header';
-import data from '../../data/data.json';
-import './style.css';
+const Home = () => {
+  const { cart } = useCart();
 
-const Home = ()=>{
-    const {cart} = useCart();
-
-    useEffect(()=>{
-        data.products.forEach((produtos,i) => {
-            // console.log(data.products[i].name);
-            })
-    },[cart]);
-    return(
-        <div className="home">
-            <Header page='home'/>
-            <div className="body-home">
-                {data.products.map((produto,i) => {
-
-                return <Product prod={produto}/>
-                })}
-            </div>
-        </div>
-    )
+  useEffect(() => {
+    data.products.forEach((produtos, i) => {});
+  }, [cart]);
+  return (
+    <div className="home">
+      <Header page="home" />
+      <div className="body-home">
+        {data.products.map((produto, i) => {
+          return <Product prod={produto} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
-export default Home
+export default Home;
